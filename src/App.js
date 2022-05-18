18fda2fd3b3996821ccde8035f65ebbbea4c30cb
abbiprema <abirami22296@gmail.com>
2022-05-17 21:19:50 -0700

    2 files modified, 4 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-import "./style.css";
-
+import React, { useState } from 'react';
+import './style.css';
+import Card from './Card';
+import Form from './Form';
+import Header from './Header';
+import Feedback from './Feedback';
 export default function App() {
+  const [getAddress, setAddress] = useState('salman@gmail.com');
+
+  const addressHandler = (event) => {
+    setAddress(event.target.value);
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="grid-container">
+      <div>
+        <Feedback getAddress={getAddress} addressHandler={addressHandler} />
+      </div>
+      <div>
+        <Card productName={getAddress} color="blue" amount="4000" />
+      </div>
     </div>
   );
 }
---
+++ src/Feedback.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+export default function Feedback(props) {
+  return (
+    <div className="feedback">
+      Email Address:
+      <input
+        type="text"
+        onChange={props.addressHandler}
+        value={props.getAddress}
+      />
+    </div>
+  );
+}
