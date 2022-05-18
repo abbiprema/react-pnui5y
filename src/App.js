545a262645aa339ba89ea8d31246fe1e018ad037
abbiprema <abirami22296@gmail.com>
2022-05-17 21:32:25 -0700

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import Form from './Form';
 import Header from './Header';
 import Feedback from './Feedback';
 export default function App() {
-  const [getAddress, setAddress] = useState('salman@gmail.com');
+  const [getAddress, setAddress] = useState('BMW Car')
+
+  const addressHandler = (event) => {
+    setAddress(event.target.value);
+  };
+  const [getAddress, setAddress] = useState('Car2')
+
+  const addressHandler = (event) => {
+    setAddress(event.target.value);
+  };
+  const [getAddress, setAddress] = useState('Car2')
 
   const addressHandler = (event) => {
     setAddress(event.target.value);
@@ -18,6 +28,12 @@ export default function App() {
       </div>
       <div>
         <Card productName={getAddress} color="blue" amount="4000" />
+      </div>
+      <div>
+        <card productName={getAddress} color="Red" amount="70000" />
+      </div>
+      <div>
+        <card productName={getAddress} color="Yellow" amount="90000" />
       </div>
     </div>
   );
