f5316c8ddf6b400af09c2c2aa6ad3da23ea2f6e7
abbiprema <abirami22296@gmail.com>
2022-05-19 09:18:59 -0700

    1 file modified, 1 added

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import './contact.css';
 
 function contact() {
@@ -6,5 +6,26 @@ function contact() {
   const [getTwo, setTwo] = useState(0);
   const [getResult, setResult] = useState(0);
 
-  const addition
+  const addition = () => {
+    alert('addition');
+    setResult(Number(getone) + Number(getTwo));
+  };
+  const onChangeHandlerOne = (event) => {
+    setOne(event.target.value);
+  };
+  const onChangeHandlerTwo = (event) => {
+    setTwo(event.target.value);
+  };
+  return (
+    <div>
+      <div className=""> Addition: {getResult} </div>
+      The Value of A:
+      <input type="text" onchange={onChangeHandlerOne} />
+      The value of B:
+      <input type="text" onchange={onChangeHandlerTwo} />
+      <button onClick={addition}>Click </button>
+    </div>
+  );
 }
+
+export default contact;
