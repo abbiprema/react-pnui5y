3654f057129ca7979e5b3762fdbc53ca0110bb98
abbiprema <abirami22296@gmail.com>
2022-05-19 08:56:49 -0700

    9 files added, 8 deleted

---
+++ src/Contact.js
@@ -0,0 +1,10 @@
+import React, { useState} from 'react';
+import './contact.css';
+
+function contact() {
+  const [getOne, setOne] = useState(0);
+  const [getTwo, setTwo] = useState(0);
+  const [getResult, setResult] = useState(0);
+
+  const addition
+}
